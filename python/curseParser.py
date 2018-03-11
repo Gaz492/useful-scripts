b3faa57f6db46621c772d2dcad2bb4c568e4c8fd
Gareth Williams <Gaz492@users.noreply.github.com>
2018-03-11 15:08:01 +0000

    Update curseParser.py

--- python/curseParser.py
+++ python/curseParser.py
@@ -1,4 +1,4 @@
-#!/root/curseParser/py3env/bin/python3
+#!/usr/bin/python
 import json
 import requests
 import hashlib
@@ -7,12 +7,9 @@ import argparse
 import time
 import datetime
 
-apiUrl = 'http://localhost:8050'
-modsFound = []
-newJson = []
+print('Script started at: ' + datetime.datetime.fromtimestamp(time.time()).strftime('%d-%m-%Y %H:%M:%S'))
 
-jsonFileName = outputpath + '/curseProjects.json'
-hashFileName = jsonFileName + '.md5'
+apiUrl = 'http://localhost:7043'
 
 
 parser = argparse.ArgumentParser()
@@ -24,12 +21,23 @@ if not args.output:
     sys.exit(2)
 else:
     outputpath = args.output
+    print('Setting output dir:' + str(outputpath))
 
 start_time = time.time()
+modsFound = []
+jsonProjects = []
+newJson = {}
 
+jsonFileName = outputpath + '/curseProjects.json'
+hashFileName = jsonFileName + '.md5'
+
+print('Requesting addon JSON')
 j = requests.get(apiUrl + '/api/addon')
 j_obj = j.json()
 
+print('Started parsing addon JSON for projects')
+
+newJson['timestamp'] = datetime.datetime.fromtimestamp(time.time()).strftime('%d-%m-%Y %H:%M:%S')
 for project in j_obj:
     if (project['PackageType'].lower() == 'mod') and (project['GameId'] == 432):
         tempJson = requests.get(apiUrl + '/api/addon/' + str(project['Id']) + '/files')
@@ -44,11 +52,12 @@ for project in j_obj:
                         'fileName': projectFile['FileNameOnDisk'],
                         'gameVersion': projectFile['GameVersion']
                     }
-                    newJson.append(tmpMod)
+                    jsonProjects.append(tmpMod)
             except TypeError:
                 print('Error on: ' + str(project['Id']))
                 pass
 
+newJson['data'] = jsonProjects
 newJson = json.dumps(newJson)
 jsonFile = open(jsonFileName, 'w')
 jsonFile.write(newJson)
@@ -61,4 +70,3 @@ hashFile = open(hashFileName, 'w')
 hashFile.write(str(fileHash.hexdigest()))
 hashFile.close()
 print("--- %s seconds ---" % str(datetime.timedelta(seconds=(time.time() - start_time))))
-
